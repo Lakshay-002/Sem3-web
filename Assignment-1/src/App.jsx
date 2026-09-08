8a54c30d30259b00d4193841f039081cd1c17a9b
Lakshay Baisla <lakshaybaisla@Lakshays-MacBook-Air-3.local>
2026-09-08 10:49:48 +0530

    Add and comment out runs for all assignment questions in App.jsx

--- Assignment-1/src/App.jsx
+++ Assignment-1/src/App.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
-import Q5 from'./Assignment-2/Q5'
+
+// --- Assignment 1 ---
+import A1Q1 from './Assignment-1/Q1'
+import A1Q2 from './Assignment-1/Q2'
+import A1Q3 from './Assignment-1/Q3'
+import A1Q4 from './Assignment-1/Q4'
+import A1Q5 from './Assignment-1/Q5'
+
+// --- Assignment 2 ---
+import A2Q1 from './Assignment-2/Q1'
+import A2Q2 from './Assignment-2/Q2'
+import A2Q3 from './Assignment-2/Q3'
+import A2Q4 from './Assignment-2/Q4'
+import A2Q5 from './Assignment-2/Q5'
+
+// --- Assignment 3 ---
+import A3Q1 from './Assignment-3/Q1'
+import A3Q2 from './Assignment-3/Q2'
+import A3Q3 from './Assignment-3/Q3'
+import A3Q4 from './Assignment-3/Q4'
+
+// --- Assignment 4 ---
+import A4Q1 from './Assignment-4/Q1'
+import A4Q2 from './Assignment-4/Q2'
+import A4Q3 from './Assignment-4/Q3'
+import A4Q4 from './Assignment-4/Q4'
+import A4Q5 from './Assignment-4/Q5'
+import A4Q6 from './Assignment-4/Q6'
+
+// --- Assignment 5 ---
+import A5Q1 from './Assignment-5/Q1'
+import A5Q2 from './Assignment-5/Q2'
+import A5Q3 from './Assignment-5/Q3'
+import A5Q4 from './Assignment-5/Q4'
+
+// --- Assignment 6 ---
+import A6Q1 from './Assignment-6/Q1'
+import A6Q2 from './Assignment-6/Q2'
+import A6Q3 from './Assignment-6/Q3'
+import A6Q4 from './Assignment-6/Q4'
+import A6Q5 from './Assignment-6/Q5'
 
 const App = () => {
   return (
     <div>
-      <Q5></Q5>
+      {/* Uncomment any question below to run it */}
+
+      {/* --- Assignment 1 --- */}
+      {/* <A1Q1 /> */}
+      {/* <A1Q2 /> */}
+      {/* <A1Q3 /> */}
+      {/* <A1Q4 /> */}
+      {/* <A1Q5 /> */}
+
+      {/* --- Assignment 2 --- */}
+      {/* <A2Q1 /> */}
+      {/* <A2Q2 /> */}
+      {/* <A2Q3 /> */}
+      {/* <A2Q4 /> */}
+      {/* <A2Q5 /> */}
+
+      {/* --- Assignment 3 --- */}
+      {/* <A3Q1 /> */}
+      {/* <A3Q2 /> */}
+      {/* <A3Q3 /> */}
+      {/* <A3Q4 /> */}
+
+      {/* --- Assignment 4 --- */}
+      {/* <A4Q1 /> */}
+      {/* <A4Q2 /> */}
+      {/* <A4Q3 /> */}
+      {/* <A4Q4 /> */}
+      {/* <A4Q5 /> */}
+      {/* <A4Q6 /> */}
+
+      {/* --- Assignment 5 --- */}
+      {/* <A5Q1 /> */}
+      {/* <A5Q2 /> */}
+      {/* <A5Q3 /> */}
+      {/* <A5Q4 /> */}
+
+      {/* --- Assignment 6 --- */}
+      {/* <A6Q1 /> */}
+      {/* <A6Q2 /> */}
+      {/* <A6Q3 /> */}
+      {/* <A6Q4 /> */}
+      {/* <A6Q5 /> */}
     </div>
   )
 }
